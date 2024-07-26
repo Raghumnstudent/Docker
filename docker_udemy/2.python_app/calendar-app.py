e3762ffb6851a84f2dee2bd42642ab51c3bb7825
Raghu M N <nraghu@zeomega.com>
2024-07-27 01:27:39 +0530

    Updated the docker notes

--- docker_udemy/2.python_app/calendar-app.py
+++
@@ -1,8 +0,0 @@
-import calendar 
-
-year = int(input("Please enter the year: "))
-month = int(input("Please enter the month: "))
-
-print(calendar.month(year, month))
-
-print("Have a nice day")
--- docker_udemy/3.node/hello.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello word executed by Node js!")
